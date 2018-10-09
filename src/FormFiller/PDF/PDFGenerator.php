0372b467a2364dac6188b8ea8e23c71688232782
Raymond Sarmiento <rsarmiento@macquariecloudservices.com>
2018-10-09 13:44:35 +0800

    Added dynamic path for the created temporary pdf

--- src/FormFiller/PDF/PDFGenerator.php
+++ src/FormFiller/PDF/PDFGenerator.php
@@ -74,7 +74,7 @@ class PDFGenerator {
      * @return bool
      * @throws \Exception
      */
-    public function start(string $formPath, string $dest, string $fontName = 'Arial', string $fontSize = '12', string $fontStyle = 'B') {
+    public function start(string $formPath, string $dest, string $tempPathAndName = '', string $fontName = 'Arial', string $fontSize = '12', string $fontStyle = 'B') {
         $this->fpdf = new FPDF($this->orientation, $this->unit, $this->size);
 
         $this->fpdf->SetMargins(10, 10, 10);
@@ -92,8 +92,8 @@ class PDFGenerator {
         // writing fields, if value not defined defaults to blank string
         $this->writeFields($this->fields, $this->data, $sizes[$this->size]);
 
-        // generated path
-        $generated = getcwd() . "/tmp/temp.pdf";
+        // generated path, default is
+        $generated = empty($tempPathAndName) ? getcwd() . "/tmp/temp.pdf" : $tempPathAndName;
 
         $this->fpdf->Output("F", $generated, true);
 
